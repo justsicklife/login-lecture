8d092e8cf0d25c822324e3075262e47a0b659e7c
justsicklife <justsicklife@gmail.com>
2024-04-09 17:56:49 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+app.set("views","./src/views");
+app.set("view engine" ,"ejs");
+
+const home = require("./src/routes/home");
+
+app.use("/",home); // use -> 미들웨어 등록하는 메서드.
+
+module.exports = app;
